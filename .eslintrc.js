28d54cbda06f692d37b67ac3d13e0933f2f1d9cd
liverpl1920 <liverpl1920@gmail.com>
2026-03-11 08:05:30 +0900

    chore: add lint and format config
    - ESLint with TypeScript, React, and react-hooks plugins
    - Prettier with single-quote, semi, 100 char print width
    - eslint-config-prettier to disable conflicting rules

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2022: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  settings: {
+    react: { version: 'detect' },
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-console': 'error',
+    'react/react-in-jsx-scope': 'off',
+  },
+};
